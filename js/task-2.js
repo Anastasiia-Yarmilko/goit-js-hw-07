27c580a0b86355bf2cf29cf7ed40921343e67b2e
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2020-11-21 20:16:01 +0200

    Выполняет задание 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,3 @@
-const ingredientItem = document.createElement('li');
-ingredientItem.textContent = '';
-
-console.log(ingredientItem);
-
-
 const ingredients = [
     'Картошка',
     'Грибы',
@@ -12,6 +6,21 @@ const ingredients = [
     'Зелень',
     'Приправы',
   ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const attachedIngredients = ingredients.map((ingredient) => {
+    const ingredientItem = document.createElement('li')
+    ingredientItem.textContent = ingredient;
+
+    return ingredientItem;
+}
+)
+
+ingredientsList.append(...attachedIngredients);
+
+
+
 //   Напиши скрипт, который для каждого элемента массива 
 // ingredients создаст отдельный li, после чего вставит все li 
 // за одну операцию в список ul.ingredients. Для создания DOM-узлов используй 
